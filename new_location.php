17e08a958c85fcc33dfac34c4d00637e9bb8cb6c
SirBob <kleonardo52@gmail.com>
2017-06-11 03:22:42 +0800

    Update new_location.php

--- new_location.php
+++ new_location.php
@@ -1,8 +1,8 @@
 <?php
-	$f = fopen($_POST['orgname'].'_branches.txt', "a+");
+	$f = fopen($_POST['name'].'_branches.txt', "a+");
 
 	fwrite($f, $_POST['name'].' ^ '.$_POST['address'].' ^ '.$_POST['operating']." ^ ".$_POST['lat']." ^ ".$_POST['lng']."\r\n");
 	
 	fclose($f);
-	header("Location: home.html");
+	header("Location: index.html");
 ?>
